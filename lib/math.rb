2544f9879479f407e397541f57f0da23b6348ea0
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-21 20:40:13 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
 def addition(num1, num2)
-
+  p num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  p num1 - num2
 end
-
+  
 def division(num1, num2)
-
+  p num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  p num1 * num2
 end
 
 def modulo(num1, num2)
-
+  p num1 % num2
 end
 
 def square_root(num)
-
+  x = Math.sqrt(num)
+  p x
 end
+
+addition(5,4)
+subtraction(10,5)
+division(50,2)
+multiplication(4,30)
+modulo(34,5)
+square_root(81)
